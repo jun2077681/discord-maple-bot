f7c76e41260a8049600edd79d6aaad8867473924
hasu <jun2077681@naver.com>
2021-06-04 10:49:06 +0900

    타이머 TTS 테스트 중

--- hillaTimer.py
+++ hillaTimer.py
@@ -1,9 +1,36 @@
-from discord.ext import commands, tasks
+import discord
+from discord.ext import commands
+from discord import FFmpegPCMAudio
+from gtts import gTTS
+import os
+import asyncio
+import datetime
+
+class TTSEngin:
+    def __init__(self, lang = 'ko'):
+        self.lang = lang
+
+    async def gen_text_to_mp3(self, text, filename = 'time.mp3'):
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+        tts = gTTS(text = text, lang = self.lang, slow=False)
+        
+        return tts
 
 class HillaTimer:
-    def __init__(self):
+    def __init__(self, vc: discord.voice_client.VoiceClient):
         self.time = 60 * 30
-        self.next_time = 29 * 60 + 14
+        self.next_time = 29 * 60 + 20
+        self.TTS = TTSEngin()
+        self.vc = vc
+        self.phase = 1
+
+        for i in os.listdir():
+            if i.endswith('.mp3'):
+                os.remove(i)
+        
+        print(f"START: {datetime.datetime.now()}")
 
     def decrease_time(self):
         self.time -= 1
@@ -11,13 +38,45 @@ class HillaTimer:
     def set_time(self, minutes, seconds):
         self.time = minutes * 60 + seconds
 
-    def check_time_range(self):
-        self.decrease_time()
-        if self.next_time <= self.time <= self.next_time + 20:
-            return True
+    def set_phase(self, phase):
+        self.phase = phase
 
-        return False
+    async def check_time_range(self, range_list: list):
+        for r in range_list:
+            if self.next_time + r == self.time:
+                print(f"{r}초 후 낫베기 {datetime.datetime.now()}")
+                await self.TTS.gen_text_to_mp3(f"{r}초 후 낫베기", 'time.mp3')
+                try:
+                    await self.vc.play(FFmpegPCMAudio(source="time.mp3"), after=lambda e: print('done', e))
+                except:
+                    pass
+
+    async def calc_next_time(self, ctx: commands.context.Context):
+        if self.phase == 1:
+            self.next_time -= 150
+        elif self.phase == 2:
+            self.next_time -= 125
+        elif self.phase == 3:
+            self.next_time -= 100
+        
+        await ctx.send(f"다음 낫베기: {self.next_time // 60}분 {self.next_time % 60}초")
+
+        print(f"다음 낫베기: {self.next_time // 60}분 {self.next_time % 60}초 {datetime.datetime.now()}")
+        await self.TTS.gen_text_to_mp3(f"{self.next_time // 60}분 {self.next_time % 60}초", 'next_time.mp3')
+        try:
+            await self.vc.play(FFmpegPCMAudio(source="next_time.mp3"), after=lambda e: print('done', e))        
+        except:
+            pass
 
     async def start_timer(self, ctx: commands.context.Context):
         self.decrease_time()
-        await ctx.send(f'남은 시간: {self.time // 60} : {self.time % 60}')
+        if self.time % 10 == 0:
+            remain_time = self.time - self.next_time
+            print(f'남은 시간: {remain_time // 60}분 {str(remain_time % 60).zfill(2)}초 ({remain_time}초) {datetime.datetime.now()}')
+            await ctx.send(f'남은 시간: {remain_time // 60}분 {str(remain_time % 60).zfill(2)}초 ({remain_time}초)')
+        await self.check_time_range([10, 30, 60])
+
+        if self.time <= self.next_time:
+            await self.calc_next_time(ctx)
+        
+        await self.TTS.gen_text_to_mp3(f"{self.time // 60}분 {self.time % 60}초")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 from discord import FFmpegPCMAudio
 from config.config import token as secret_token
 import datetime
@@ -8,6 +8,8 @@ from hillaTimer import HillaTimer
 
 bot = commands.Bot(command_prefix = '!')
 scheduler = AsyncIOScheduler()
+hilla_timer: HillaTimer = None
+vc: discord.voice_client.VoiceClient = None
 
 @bot.event
 async def on_ready():
@@ -23,24 +25,49 @@ async def 종료(ctx: commands.context.Context):
     await bot.logout()
 
 @bot.command()
-async def 진힐라(ctx: commands.context.Context, command: str = ''):
+async def 진힐라(ctx: commands.context.Context, command: str = '시작'):
+    print(f"command: {command}")
+    
+    global hilla_timer
+    global vc
+    
     if command == '종료':
         scheduler.shutdown()
         scheduler.remove_all_jobs()
+        hilla_timer = None
+        if vc:
+            await vc.disconnect()
     elif command == '시작':
-        hilla_timer = HillaTimer()
+        try:
+            if ctx.author.voice:
+                vc = await ctx.author.voice.channel.connect()
+            else:
+                raise AttributeError
+        except AttributeError:
+            await ctx.send("음성채널 연결을 확인하세요.")
+            return
+
+        if hilla_timer == None:
+            hilla_timer = HillaTimer(vc)
         scheduler.add_job(hilla_timer.start_timer, 'interval', seconds = 1, id = 'hilla_timer', args=(ctx, ))
         scheduler.start()
     elif command == '1':
+        hilla_timer.set_phase(1)
         return
     elif command == '2':
+        hilla_timer.set_phase(2)
         return
     elif command == '3':
+        hilla_timer.set_phase(3)
         return
 
 @bot.command()
 async def 테스트(ctx: commands.context.Context):
     vc: discord.voice_client.VoiceClient = await ctx.author.voice.channel.connect()
-    vc.play(FFmpegPCMAudio(executable=r"ffmpeg-4.4-essentials_build/bin/ffmpeg.exe", source='test.mp3'), after=lambda e: print('done', e))
+    
+    hilla_timer = HillaTimer(vc)
+    hilla_timer.TTS.gen_text_to_mp3(f"{r}초 후 낫베기", 'time.mp3')
+
+    vc.play(FFmpegPCMAudio(source='hello.mp3'), after=lambda e: print('done', e))
 
 bot.run(secret_token)
